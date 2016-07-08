694d14d4c3ba739c6a452d3e449dc3b2c54e1af6
rajeshaz09 <rajesh.aravapalli@Rajesh-Aravapalli-MacBook-Pro.local>
2016-07-08 10:21:42 +0530

    Initial commit

---
+++ examples/typescriptcounter/README.md
@@ -0,0 +1,24 @@
+# Redux DevTools Counter example
+
+## Running Example
+
+First, clone the project:
+
+```
+git clone https://github.com/rajeshaz09/redux-devtools.git
+
+
+
+Install the dependencies in the example folder:
+
+```
+cd examples/typescriptcounter
+npm install
+```
+
+Finally, run the project:
+
+```
+npm start
+open http://localhost:3000
+```
---
+++ examples/typescriptcounter/src/containers/DevTools.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import SliderMonitor from 'redux-slider-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
+import Inspector from 'redux-devtools-inspector';
+import DiffMonitor from 'redux-devtools-diff-monitor';
+import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
+import ChartMonitor from 'redux-devtools-chart-monitor';
+
+
+export default createDevTools(
+  <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q' changeMonitorKey='ctrl-m'>
+    <LogMonitor />
+    <SliderMonitor />
+    <Inspector />
+    <DiffMonitor theme='tomorrow' />
+    <FilterableLogMonitor />
+    <ChartMonitor />
+  </DockMonitor>
+);
