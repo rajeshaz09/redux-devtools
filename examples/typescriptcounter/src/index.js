68d207ef6ce724f59470ec294876e792f0fb90b1
rajeshaz09 <rajesh.aravapalli@Rajesh-Aravapalli-MacBook-Pro.local>
2016-07-11 11:23:35 +0530

    updated

--- examples/typescriptcounter/src/index.js
+++ examples/typescriptcounter/src/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import configureStore from './store/configureStore';
+import Root from './containers/Root';
 
 const store = configureStore();
-const myRender = () => {
-  const Root= require('./containers/Root').default;
-  console.log('render root')
-  render(
-    <AppContainer>
-      <Root
-        store={ store }
-        />
-    </AppContainer>,
-    document.getElementById('root')
-  );
-}
+
+render(
+  <AppContainer>
+    <Root
+      store={ store }
+      />
+    />
+>>>>>>> gaearon/master
+  </AppContainer>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
-  myRender();
-  module.hot.accept('./containers/Root', myRender);
-}
-else {
-  render(<Root store={ store }/>);
+  module.hot.accept('./containers/Root', () => {
+    const RootContainer = require('./containers/Root').default;
+    render(
+      <AppContainer>
+        <RootContainer
+          store={ store }
+        />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
 }
