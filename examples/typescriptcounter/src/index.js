7f100998f10a26e568c2601e1563743d00a90ecc
rajeshaz09 <rajesh.aravapalli@Rajesh-Aravapalli-MacBook-Pro.local>
2016-07-11 11:01:44 +0530

    fixed hmr

--- examples/typescriptcounter/src/index.js
+++ examples/typescriptcounter/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import configureStore from './store/configureStore';
-import Root from './containers/Root';
 
 const store = configureStore();
 const myRender = () => {
-  console.log('render')
+  const Root= require('./containers/Root').default;
+  console.log('render root')
   render(
     <AppContainer>
       <Root
